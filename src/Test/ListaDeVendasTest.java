542fe88bac7c1810951f41eb9f7f615b6660a4e0
Joseph Affeldt <joseph.affeldt@dbccompany.com.br>
2020-07-30 19:40:11 -0300

    criar testes para lista de vendas.

--- src/Test/ListaDeVendasTest.java
+++ src/Test/ListaDeVendasTest.java
@@ -1,6 +1,9 @@
 package Test;
 
+import AgiBank.*;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -8,33 +11,188 @@ public class ListaDeVendasTest {
 
     @Test
     public void quantidadeDeVendas() {
+        ListaDeVendas lv = new ListaDeVendas();
+        ArrayList<Item> itens = new ArrayList<Item>();
+        Item it1 = new Item("1", 2, 3.0);
+        Item it2 = new Item("2", 3, 3.0);
+        Item it3 = new Item("3", 4, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor = new Vendedor("02", "Jose", 2000);
+        Venda venda = new Venda("01", itens, vendedor);
+        lv.adicionaVendaNaLista(venda);
+
+        itens.clear();
+
+        it1 = new Item("1", 5, 5.0);
+        it2 = new Item("2", 4, 4.0);
+        it3 = new Item("3", 3, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor2 = new Vendedor("03", "Joao", 3000);
+        Venda venda2 = new Venda("02", itens, vendedor2);
+        lv.adicionaVendaNaLista(venda2);
+
+        assertEquals(2, lv.quantidadeDeVendas());
     }
 
     @Test
     public void adicionaVendaNaLista() {
+        ListaDeVendas lv = new ListaDeVendas();
+        ArrayList<Item> itens = new ArrayList<Item>();
+        Item it1 = new Item("1", 2, 3.0);
+        Item it2 = new Item("2", 3, 3.0);
+        Item it3 = new Item("3", 4, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor = new Vendedor("02", "Jose", 2000);
+        Venda venda = new Venda("01", itens, vendedor);
+
+        assertEquals(true, lv.adicionaVendaNaLista(venda));
+    }
+
+    @Test
+    public void adicionaVendaExistenteNaLista() {
+        ListaDeVendas lv = new ListaDeVendas();
+        ArrayList<Item> itens = new ArrayList<Item>();
+        Item it1 = new Item("1", 2, 3.0);
+        Item it2 = new Item("2", 3, 3.0);
+        Item it3 = new Item("3", 4, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor = new Vendedor("02", "Jose", 2000);
+        Venda venda = new Venda("01", itens, vendedor);
+        lv.adicionaVendaNaLista(venda);
+        Venda venda2 = new Venda ("01", itens, vendedor);
+
+        assertEquals(false, lv.adicionaVendaNaLista(venda2));
     }
 
     @Test
     public void buscaVendaPelaIdSale() {
+        ListaDeVendas lv = new ListaDeVendas();
+        ArrayList<Item> itens = new ArrayList<Item>();
+        Item it1 = new Item("1", 2, 3.0);
+        Item it2 = new Item("2", 3, 3.0);
+        Item it3 = new Item("3", 4, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor = new Vendedor("02", "Jose", 2000);
+        Venda venda = new Venda("01", itens, vendedor);
+        lv.adicionaVendaNaLista(venda);
+
+        assertEquals(venda, lv.buscaVendaPelaIdSale("01"));
+    }
+
+    @Test
+    public void buscaVendaNaoExistentePelaIdSale() {
+        ListaDeVendas lv = new ListaDeVendas();
+        assertNull(lv.buscaVendaPelaIdSale("01"));
     }
 
     @Test
     public void criaListaDeItens() {
+        ListaDeVendas lv = new ListaDeVendas();
+        ArrayList<Item> itens = new ArrayList<Item>();
+        Item it1 = new Item("1", 2, 3.0);
+        Item it2 = new Item("2", 3, 3.0);
+        Item it3 = new Item("3", 4, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+
+        String str = "[1-2-3.0,2-3-3.0,3-4-3.0]";
+
+        assertEquals(itens, lv.criaListaDeItens(str));
     }
 
     @Test
     public void criaItemDaVenda() {
+        ListaDeVendas lv = new ListaDeVendas();
+        Item it1 = new Item("1", 2, 3.0);
+
+        String str = "1-2-3.0";
+
+        assertEquals(it1, lv.criaItemDaVenda(str));
     }
 
     @Test
     public void existeVenda() {
+        ListaDeVendas lv = new ListaDeVendas();
+        ArrayList<Item> itens = new ArrayList<Item>();
+        Item it1 = new Item("1", 2, 3.0);
+        Item it2 = new Item("2", 3, 3.0);
+        Item it3 = new Item("3", 4, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor = new Vendedor("02", "Jose", 2000);
+        Venda venda = new Venda("01", itens, vendedor);
+        lv.adicionaVendaNaLista(venda);
+
+        assertEquals(true, lv.existeVenda());
     }
 
     @Test
     public void vendaMaisCara() {
+        ListaDeVendas lv = new ListaDeVendas();
+        ArrayList<Item> itens = new ArrayList<Item>();
+        Item it1 = new Item("1", 2, 3.0);
+        Item it2 = new Item("2", 3, 3.0);
+        Item it3 = new Item("3", 4, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor = new Vendedor("02", "Jose", 2000);
+        Venda venda = new Venda("01", itens, vendedor);
+        lv.adicionaVendaNaLista(venda);
+
+        itens.clear();
+
+        it1 = new Item("1", 5, 5.0);
+        it2 = new Item("2", 4, 4.0);
+        it3 = new Item("3", 3, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor2 = new Vendedor("03", "Joao", 3000);
+        Venda venda2 = new Venda("02", itens, vendedor2);
+        lv.adicionaVendaNaLista(venda2);
+
+        assertEquals(venda2, lv.vendaMaisCara());
     }
 
     @Test
     public void piorVendedor() {
+        ListaDeVendas lv = new ListaDeVendas();
+        ArrayList<Item> itens = new ArrayList<Item>();
+        Item it1 = new Item("1", 2, 3.0);
+        Item it2 = new Item("2", 3, 3.0);
+        Item it3 = new Item("3", 4, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor = new Vendedor("02", "Jose", 2000);
+        Venda venda = new Venda("01", itens, vendedor);
+        lv.adicionaVendaNaLista(venda);
+
+        itens.clear();
+
+        it1 = new Item("1", 5, 5.0);
+        it2 = new Item("2", 4, 4.0);
+        it3 = new Item("3", 3, 3.0);
+        itens.add(it1);
+        itens.add(it2);
+        itens.add(it3);
+        Vendedor vendedor2 = new Vendedor("03", "Joao", 3000);
+        Venda venda2 = new Venda("02", itens, vendedor2);
+        lv.adicionaVendaNaLista(venda2);
+
+        assertEquals(vendedor, lv.piorVendedor());
     }
 }
